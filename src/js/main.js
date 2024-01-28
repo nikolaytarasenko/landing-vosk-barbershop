52ccf3c329690f6234271145e0a1b4703cbc7921
Mykola Tarasenko <kolyatarasenko@ukr.net>
2024-01-28 22:03:59 +0200

    add mobile version and smooth scroll to section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-// import $ from 'jquery';
-import AOS from 'aos';
 import 'fslightbox'
 
 const initStickyHeader = e => {
@@ -10,29 +8,66 @@ const initStickyHeader = e => {
   document.body.classList.toggle('scrolled', scrollY > 0);
 }
 
+const initMobileMenu = () => {
+  const header = document.querySelector('.header');
+  const burgerButton = document.querySelector('.burger');
+
+  burgerButton.addEventListener('click', function(e) {
+    header.classList.toggle('menu-open');
+    document.body.classList.toggle('overflow-hidden');
+  })
+}
+
+const closeMobileMenu = () => {
+  if (window.matchMedia('(min-width: 992px)').matches) {
+    const header = document.querySelector('.header');
+
+    header.classList.remove('menu-open');
+    document.body.classList.remove('overflow-hidden');
+  }
+}
+
 const hidePreloader = () => {
   const preloaderWrapper = document.querySelector('.preloader-wrapper');
 
   preloaderWrapper.classList.add('hide');
 }
 
-const resizeHandler = () => {
+const scrollToSection = () => {
+  const menuLinks = document.querySelectorAll('.nav__link');
 
+  menuLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      if (!e.target.getAttribute('href').includes('#')) return;
+
+      e.preventDefault();
+
+      const header = document.querySelector('.header');
+      const anchor = e.target.getAttribute('href').slice(1);
+      const targetSection = document.querySelector(`#${anchor}`);
+
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+
+      header.classList.remove('menu-open');
+      document.body.classList.remove('overflow-hidden');
+    });
+  });
+}
+
+const resizeHandler = () => {
+  closeMobileMenu();
 }
 
 const domContentLoadedHandler = () => {
+  initMobileMenu();
+  scrollToSection();
+
   window.addEventListener('scroll', initStickyHeader);
 }
 
 const loadHandler = () => {
-  AOS.init({
-    duration: 500,
-    easing: "ease-in-out",
-    once: true,
-    mirror: false,
-    disable: window.innerWidth < 768
-  });
-
   hidePreloader();
 }
 
